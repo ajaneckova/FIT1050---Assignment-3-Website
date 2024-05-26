9f2f88c0bc136352c402f517cdfaf06dfafab122
ajaneckova <160802003+ajaneckova@users.noreply.github.com>
2024-05-26 22:50:07 +1000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+// add classes for mobile navigation toggling
+var CSbody = document.querySelector('body');
+const CSnavbarMenu = document.querySelector('#cs-navigation');
+const CShamburgerMenu = document.querySelector('#cs-navigation .cs-toggle');
+
+CShamburgerMenu.addEventListener('click', function () {
+	CShamburgerMenu.classList.toggle('cs-active');
+	CSnavbarMenu.classList.toggle('cs-active');
+	CSbody.classList.toggle('cs-open');
+	// run the function to check the aria-expanded value
+	ariaExpanded();
+});
+
+// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not
+function ariaExpanded() {
+	const csUL = document.querySelector('#cs-expanded');
+	const csExpanded = csUL.getAttribute('aria-expanded');
+
+	if (csExpanded === 'false') {
+		csUL.setAttribute('aria-expanded', 'true');
+	} else {
+		csUL.setAttribute('aria-expanded', 'false');
+	}
+}
+
+// mobile nav toggle code
+const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
+for (const item of dropDowns) {
+	const onClick = () => {
+		item.classList.toggle('cs-active');
+	};
+	item.addEventListener('click', onClick);
+}
+
+
+Fancybox.bind( '[data-fancybox]' );
+
+$( 'button' ).click( filterItems );
+
+
+function filterItems(){
+    let button = this;
+    let selector = $( button ).data( 'filter' );
+
+    //console.log( selector );]
+
+    $( '#products img' ).not( selector ).hide( 500 );
+    $( selector ).show( 500 );
+
+}
